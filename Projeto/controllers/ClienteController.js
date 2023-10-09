c555a26f799fc4f3ebc9c7242a4903f5f9ce7900
Lucas-And3rson <lucasestuda09@gmail.com>
2023-10-09 10:20:03 -0300

    atualizar

--- Projeto/controllers/ClienteController.js
+++ Projeto/controllers/ClienteController.js
@@ -1,9 +1,33 @@
 const ClienteModel = require("../models/ClienteModel");
 const Cliente = require("../Cliente");
+const { findOneAndUpdate } = require("../models/ClienteModel");
 const VClientes = [];
 
 
 class ClienteController{
+    static async cadastrar(req, res){
+        // const NovoCliente = req.body;
+        // console.log(NovoCliente)
+        // VClientes.push(new Cliente(NovoCliente.id, NovoCliente.nome, NovoCliente.idade));
+        // res.redirect("./clientes?s=1");
+        if(req.body._id == ''){ //cadastrar
+            const novoCliente = new ClienteModel ({
+                id: req.body.id,
+                nome: req.body.nome,
+                idade: req.body.idade
+            })
+            await novoCliente.save();
+            res.redirect("/clientes?s=1");
+        }else{ //atualizar
+            const id = req.body.id;
+            const clienteUpdate = {
+                nome: req.body.nome,
+                idade: req.body.idade
+            }
+            await ClienteModel.findOneAndUpdate({id:id}, clienteUpdate);
+            res.redirect("/clientes?s=3");
+        }
+        }
     static async relatorio(req, res){
         const status = req.query.s;
         const VClientes = await ClienteModel.find();
@@ -15,25 +39,18 @@ class ClienteController{
         res.render("cliente/detalhar", {c});
 }
     static cadastrarRender(req, res){
-        res.render("cliente/cadastrar");
+        let clienteUpdate = {}
+        res.render("cliente/cadastrar", {clienteUpdate});
 }
-    static async cadastrar(req, res){
-    // const NovoCliente = req.body;
-    // console.log(NovoCliente)
-    // VClientes.push(new Cliente(NovoCliente.id, NovoCliente.nome, NovoCliente.idade));
-    // res.redirect("./clientes?s=1");
-    const novoCliente = new ClienteModel ({
-         id: req.body.id,
-         nome: req.body.nome,
-         idade: req.body.idade
-      })
-      await novoCliente.save();
-     res.redirect("/clientes?s=1");
-    }
     static async deletar(req, res){
             const id = req.params.id;
             await ClienteModel.findOneAndDelete({id:id});
             res.redirect("/clientes?s=2");
+        }
+    static async atualizar(req, res){
+            const id = req.params.id;
+            const clienteUpdate = await ClienteModel.findOne({id:id});
+            res.render("cliente/cadastrar", {clienteUpdate});
         }
 }
 
--- Projeto/routes/ClienteRoutes.js
+++ Projeto/routes/ClienteRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const routes = express.Router();
 const ClienteController = require('../controllers/ClienteController');
 
+
 routes.get("/clientes", ClienteController.relatorio);
 routes.get("/cliente/:id", ClienteController.detalhar);
-routes.get("/clientes/cadastrar", ClienteController.cadastrarRender);
-routes.get("/cliente/deletar/:id", ClienteController.deletar);
 routes.post("/clientes", ClienteController.cadastrar);
-
+routes.get("/clientes/cadastrar", ClienteController.cadastrarRender);
+routes.get("/cliente/cadastrar/:id", ClienteController.atualizar);
+routes.get("/cliente/deletar/:id", ClienteController.deletar);
 
 module.exports = routes;
