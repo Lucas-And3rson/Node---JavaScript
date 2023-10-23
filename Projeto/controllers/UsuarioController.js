9d4a17ffe6570e5cd9a248af8b73e74a5abf4c5f
Lucas-And3rson <lucasestuda09@gmail.com>
2023-10-23 10:20:46 -0300

    email existe
    atualizar e cadastrar

--- Projeto/controllers/UsuarioController.js
+++ Projeto/controllers/UsuarioController.js
@@ -11,22 +11,35 @@ class UsuarioController{
         // Vusuarios.push(new Usuario(NovoUsuario.id, NovoUsuario.nome, NovoUsuario.idade));
         // res.redirect("./usuarios?s=1");
         if(req.body._id == ''){ //cadastrar
-            const novoUsuario = new UsuarioModel ({
-                nome: req.body.nome,
-                email: req.body.email,
-                senha: req.body.senha
-            })
-            await novoUsuario.save();
-            res.redirect("/usuarios?s=1");
-        }else{ //atualizar
-            const id = req.body._id;
-            const usuarioUpdate = {
-                nome: req.body.nome,
-                email: req.body.email,
-                senha: req.body.senha
+            const user = await UsuarioModel.findOne({ email: req.body.email });
+            if(user != null){
+                //email existe
+                res.redirect(`usuarios/cadastrar?s=4&nome=${req.body.nome}&email=${req.body.email}`);
+            }else{
+                const novoUsuario = new UsuarioModel ({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                })
+                await novoUsuario.save();
+                res.redirect("/usuarios?s=1");
             }
-            await UsuarioModel.findOneAndUpdate({_id:id}, usuarioUpdate);
-            res.redirect("/usuarios?s=3");
+            
+        }else{ //atualizar
+            const user = await UsuarioModel.findOne({ email: req.body.email });
+            console.log(user)
+            console.log(req.body.email)
+            if(user == null || user.email == req.body.email){
+                console.log(1)
+                const id = req.body._id;
+                const usuarioUpdate = {
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                }
+                await UsuarioModel.findOneAndUpdate({_id:id}, usuarioUpdate);
+                res.redirect("/usuarios?s=3");
+            }
         }
         }
     static async relatorio(req, res){
@@ -40,18 +53,24 @@ class UsuarioController{
         res.render("usuario/detalhar", {u});
 }
     static cadastrarRender(req, res){
-        let usuarioUpdate = {}
-        res.render("usuario/cadastrar", {usuarioUpdate});
+        const id = req.params.id;
+        const status = req.query.s;
+        let usuarioUpdate = {
+            nome: req.query.nome,
+            email: req.query.email
+        };
+        res.render("usuario/cadastrar", {usuarioUpdate, status});
 }
     static async deletar(req, res){
-            const id = req.params.id;
-            await UsuarioModel.findOneAndDelete({_id:id});
-            res.redirect("/usuarios?s=2");
+        const id = req.params.id;
+        await UsuarioModel.findOneAndDelete({_id:id});
+        res.redirect("/usuarios?s=2");
         }
     static async atualizar(req, res){
-            const id = req.params.id;
-            const usuarioUpdate = await UsuarioModel.findOne({_id:id});
-            res.render("usuario/cadastrar", {usuarioUpdate});
+        const status = req.query.s;
+        const id = req.params.id;
+        const usuarioUpdate = await UsuarioModel.findOne({_id:id});
+        res.render("usuario/cadastrar", {usuarioUpdate, status});
         }
 }
 
